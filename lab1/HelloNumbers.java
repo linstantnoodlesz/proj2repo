9ddc32ec3a15fdbb72ee047e05cc28cf57480d8e
Joseph <joseph.pagadora.c@gmail.com>
2017-01-17 18:01:22 -0800

    Finished HelloNumbers main method and LeapYear isLeapYear method.

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,17 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int i, sum;
+        while (x <= 10) {
+            i = 0;
+            sum = 0;
+            while (i <= x) {
+            	sum += i;
+            	i += 1;
+            }
+            System.out.print(sum + " ");
+            x += 1;
+        }
+        System.out.println("");
+    }
+}
